b23bb1d6183344f8662ce0a645e264e2e90c1c85
gitgud16 <uchiha.axx@gmail.com>
2024-10-03 15:53:26 +0300

    frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+</script>
+  
+
+<div class="min-h-screen bg-black text-white">
+
+    <slot />
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Meteors from '$lib/animation/Meteors.svelte';
+</script>
+
+<div class="flex flex-col min-h-screen place-items-center justify-start pt-10 ">
+
+    <img src="/TerraTales.png" class="w-fit h-fit" alt="TerraTales Logo">
+	<Meteors number={100} text="TerraTales" />
+
+</div>
+
+
