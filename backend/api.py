e91174b7c45eae50a3587ba733875f5a4652f72b
iWaheeb <waheebbasalem@gmail.com>
2024-10-05 14:48:02 +0300

    Added: New endpoint to get data of one day

--- backend/api.py
+++ backend/api.py
@@ -6,14 +6,23 @@ app = FastAPI()
 aot_data = []
 
 with open('backend/aerosol_data_riyadh.csv', mode='r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)  # Read and skip the header
-    for row in csv_reader:
+    csv_data = csv.reader(file)
+    next(csv_data)  # Read and skip the header
+    for row in csv_data:
         aot_data.append(row)  # Append the row directly (as a list)
 
-@app.get("/get-aot")
-def get_aot():
+@app.get("/data/get-all")
+def get_all():
     return aot_data
+
+
+@app.post("/data/get-one")
+def get_one(date: str):
+    date_values = []
+    for row in aot_data:
+        if row[0] == date:
+            date_values.append(row)
+    return date_values
 
 if __name__ == "__main__":
     import uvicorn
